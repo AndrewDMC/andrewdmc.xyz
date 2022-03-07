e5c5df330ff847c3ec0f4ed62dac59aaf64a31ea
Andro434 <52075779+Andro434@users.noreply.github.com>
2022-03-07 10:22:48 +0100

    Update script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav-bar") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-bar";
-  }
-}
